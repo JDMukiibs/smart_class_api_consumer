11a1db74a527cbd7b85f9f3360bfb378c89316b7
Joshua D. Mukiibi <joshuamukiibi10@gmail.com>
2021-07-23 17:21:58 +0300

    Implemented Add Images function and edited README.md
    Images are being added but I wonder if they are being saved permanently. When I went back to the homepage and went back to images they weren't there so I might need to find a way to make the images remain or when all functionality is implemented once the user has added images they will click send and once they are sent the app does indeed forget about their existence and clears its cache. Hmm? *thinking emoji*

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ Later on, the API will provide the app with an endpoint to have a metric report 
 
 
 ## Detailed information
+
+First off, I made some designs using Figma. Those can be viewed here:
+
+```HTML
+<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FpB7TcjGzSjkruTI1cV6GVj%2FSmartClass-API-Consumer%3Fnode-id%3D13%253A576" allowfullscreen></iframe>
+```
+
+At the moment of writing, still need to add code that implements complete desired functionality. 
+Latest commit will get you up to speed with where I am at.
+
--- lib/pages/send_images.dart
+++ lib/pages/send_images.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_fadein/flutter_fadein.dart';
-//import 'package:image_picker/image_picker.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SendImages extends StatefulWidget {
   const SendImages({Key? key}) : super(key: key);
@@ -11,19 +13,38 @@ class SendImages extends StatefulWidget {
 
 class _SendImagesState extends State<SendImages> {
 
-  //List<XFile>? _imageFileList; // list of images added by user
+  late XFile imageFile;
+  final ImagePicker _picker = ImagePicker();
+  List <String> imagePaths = [];
 
-  //final ImagePicker _picker = ImagePicker();
+  void chooseImage(ImageSource source) async {
+    final pickedFile = await _picker.pickImage(source: source);
+    setState(() {
+      imageFile = pickedFile!;
+      imagePaths.add(imageFile.path);
+    });
+  }
 
-  List <String> images = [
-    'assets/stock_photo_1.jpg',
-    'assets/stock_photo_2.jpg',
-    'assets/stock_photo_3.jpg',
-    'assets/stock_photo_4.jpg'
-  ];
-
-
-
+  // Created my own grid building widget function to avoid overpopulating the main function with code.
+  Widget imageTemplate(images) {
+    return GridView.builder(
+      itemCount: images.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          child: FadeIn(
+              duration: Duration(milliseconds: 500),
+              curve: Curves.easeIn,
+              child: Image.file(File(images[index]), fit: BoxFit.cover,)
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,33 +70,30 @@ class _SendImagesState extends State<SendImages> {
         children: <Widget>[
           SizedBox(height: 10),
           Expanded(
-            child: Container(
+            child: Padding(
               padding: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Colors.teal[100],
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-              child: GridView.builder(
-                itemCount: images.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
+              child: Container(
+                //padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Colors.teal[100],
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
                 ),
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    child: FadeIn(
-                      duration: Duration(milliseconds: 250),
-                      curve: Curves.easeIn,
-                      child: Image(
-                        image: AssetImage(images[index]),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  );
-                },
-              )
-              ),
+                child: imagePaths.length == 0
+                  ? Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'No images added yet!',
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                        textAlign: TextAlign.center,
+                      )
+                    ]
+                  )
+                    : imageTemplate(imagePaths)
+                ),
+            ),
           ),
           SizedBox(height: 10),
           Row(
@@ -83,7 +101,7 @@ class _SendImagesState extends State<SendImages> {
             children: [
               TextButton.icon(
                 onPressed: () {
-                  print('Add images pressed');
+                  chooseImage(ImageSource.gallery);
                 },
                 icon: Icon(
                   Icons.upload_file,
