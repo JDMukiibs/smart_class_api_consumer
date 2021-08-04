4163e9a76cb159531a2a37b772659ef696dda920
Joshua D. Mukiibi <joshuamukiibi10@gmail.com>
2021-08-04 17:20:51 +0300

    Made some updates to UI
    Today mostly begun working on the circle shapes we saw at the bottom and also adjusting and improving my code moving from guessing numbers for container heights to using MediaQuery so that the app is responsive to different screens. Major focus is screen sizes not orientations so for now it's a portrait strictly app but realized I had forgotten there are different sizes of screens. So onwards, a lot of refactoring of code to make it easier to adjust will be done.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:smart_class_api_consumer/custom_widgets/svg_generator.dart';
+import 'package:smart_class_api_consumer/helper_widgets_and_functions/shapes.dart';
+import 'package:smart_class_api_consumer/helper_widgets_and_functions/svg_generator.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,123 +16,116 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.cyan[50],
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.fromLTRB(19, 30, 280, 20),
-              child: Text(
-                'Home',
-                style: TextStyle(
-                  fontFamily: 'fonts/Poppins-Bold',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 28.0,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20.0, 0, 20, 10),
-              child: InkWell(
-                // When the container is tapped navigate to the Report screen
-                onTap: () {
-                  Navigator.pushNamed(context, '/reports');
-                },
-                child: Container(
-                  padding: EdgeInsets.fromLTRB(10, 0, 20, 0),
-                  height: 127,
-                  decoration: BoxDecoration(
-                    color: Colors.teal[100],
-                    borderRadius: BorderRadius.all(Radius.circular(20))
+        child: CustomPaint(
+          painter: CurvePainter(),
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(19, 30, 280, 20),
+                  child: Text(
+                    'Home',
+                    style: TextStyle(
+                      fontFamily: 'fonts/Poppins-Bold',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28.0,
+                      color: Colors.black,
+                    ),
                   ),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Text(
-                          'Metric Report',
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0, 0, 20, 10),
+                  child: InkWell(
+                    // When the container is tapped navigate to the Report screen
+                    onTap: () {
+                      Navigator.pushNamed(context, '/reports');
+                    },
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(10, 0, 20, 0),
+                      height: 127,
+                      decoration: BoxDecoration(
+                        color: Colors.teal[100],
+                        borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Text(
+                              'Metric Report',
+                              style: TextStyle(
+                                fontFamily: 'fonts/Poppins-Medium',
+                                fontSize: 20.0,
+                                color: Colors.black,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          Expanded(child: metricReportsSVG),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0, 0, 20, 15),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/images');
+                    },
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(10, 0, 20, 0),
+                      height: 127,
+                      decoration: BoxDecoration(
+                          color: Colors.teal[100],
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Text(
+                              'Images',
+                              style: TextStyle(
+                                fontFamily: 'fonts/Poppins-Medium',
+                                fontSize: 20.0,
+                                color: Colors.black,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          Expanded(child: imagesSVG),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0, 0, 20, 15),
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(10, 0, 20, 0),
+                    height: (MediaQuery.of(context).size.height - 200)/2,
+                    decoration: BoxDecoration(
+                        color: Colors.teal[100],
+                        borderRadius: BorderRadius.all(Radius.circular(20))
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Recent Report',
                           style: TextStyle(
                             fontFamily: 'fonts/Poppins-Medium',
                             fontSize: 20.0,
                             color: Colors.black,
                           ),
-                          textAlign: TextAlign.center,
                         ),
-                      ),
-                      Expanded(child: metricReportsSVG),
-                    ],
+                        Expanded(child: recentSVG),
+                      ],
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20.0, 0, 20, 15),
-              child: InkWell(
-                onTap: () {
-                  Navigator.pushNamed(context, '/images');
-                },
-                child: Container(
-                  padding: EdgeInsets.fromLTRB(10, 0, 20, 0),
-                  height: 127,
-                  decoration: BoxDecoration(
-                      color: Colors.teal[100],
-                      borderRadius: BorderRadius.all(Radius.circular(20))
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Text(
-                          'Images',
-                          style: TextStyle(
-                            fontFamily: 'fonts/Poppins-Medium',
-                            fontSize: 20.0,
-                            color: Colors.black,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      Expanded(child: imagesSVG),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20.0, 0, 20, 15),
-                child: Container(
-                  padding: EdgeInsets.fromLTRB(10, 0, 20, 0),
-                  height: 127,
-                  decoration: BoxDecoration(
-                      color: Colors.teal[100],
-                      borderRadius: BorderRadius.all(Radius.circular(20))
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Recent Report',
-                        style: TextStyle(
-                          fontFamily: 'fonts/Poppins-Medium',
-                          fontSize: 20.0,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Expanded(child: recentSVG),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              alignment: Alignment(0.0, -1.0),
-              child: Row(
-                children: [
-                  Text(
-                    'Footer for shapes',
-                  )
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       )
     );
