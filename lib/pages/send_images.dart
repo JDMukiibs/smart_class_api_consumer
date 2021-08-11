ce15c340555bf3b1d07579752e100ffb2f35126d
Joshua D. Mukiibi <joshuamukiibi10@gmail.com>
2021-08-11 18:06:02 +0300

    Implemented new Image picking functionality
    With the FAB in place now have implemented a new function that allows users to pick multiple images or one and also to take pictures.

--- lib/pages/send_images.dart
+++ lib/pages/send_images.dart
@@ -17,9 +17,49 @@ class _SendImagesState extends State<SendImages> {
 
   late XFile imageFile;
   late List <XFile?> images = [];
+  List <XFile>? _imageFileList;
+
+  set _imageFile(XFile? value) {
+    _imageFileList = value == null ? null : [value];
+  }
+
+  dynamic _pickImageError;
+
   final ImagePicker _picker = ImagePicker();
   List <String> imagePaths = [];
-  static const _actionTitles = ['Upload Photo(s)', 'Take a Photo'];
+  //static const _actionTitles = ['Upload Photo(s)', 'Take a Photo'];
+
+  void _onImageButtonPressed(ImageSource source, {bool isMultiImage = false}) async {
+    if(isMultiImage) {
+      try {
+        final pickedFileList = await _picker.pickMultiImage();
+        setState(() {
+          _imageFileList = pickedFileList;
+          for (var element in _imageFileList!) {
+            imagePaths.add(element.path);
+          }
+        });
+      } catch (e) {
+        setState(() {
+          _pickImageError = e;
+          print(_pickImageError);
+        });
+      }
+    } else {
+      try {
+        final pickedFile = await _picker.pickImage(source: source);
+        setState(() {
+          _imageFile = pickedFile;
+          imagePaths.add(pickedFile!.path);
+        });
+      } catch (e) {
+        setState(() {
+          _pickImageError = e;
+          print(_pickImageError);
+        });
+      }
+    }
+  }
 
   void chooseImage(ImageSource source) async {
     final pickedFile = await _picker.pickImage(source: source);
@@ -31,22 +71,22 @@ class _SendImagesState extends State<SendImages> {
     });
   }
 
-  void _showAction(BuildContext context, int index) {
-    showDialog<void>(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          content: Text(_actionTitles[index]),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('CLOSE'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+  // void _showAction(BuildContext context, int index) {
+  //   showDialog<void>(
+  //     context: context,
+  //     builder: (context) {
+  //       return AlertDialog(
+  //         content: Text(_actionTitles[index]),
+  //         actions: [
+  //           TextButton(
+  //             onPressed: () => Navigator.of(context).pop(),
+  //             child: const Text('CLOSE'),
+  //           ),
+  //         ],
+  //       );
+  //     },
+  //   );
+  // }
 
   // Created my own grid building widget function to avoid overpopulating the main function with code.
   Widget imageTemplate(images) {
@@ -220,11 +260,11 @@ class _SendImagesState extends State<SendImages> {
         distance: 112.0,
         children: [
           ActionButton(
-            onPressed: () => _showAction(context, 0),
+            onPressed: () => _onImageButtonPressed(ImageSource.gallery, isMultiImage: true),
             icon: const Icon(Icons.photo_library),
           ),
           ActionButton(
-            onPressed: () => _showAction(context, 1),
+            onPressed: () => _onImageButtonPressed(ImageSource.camera),
             icon: const Icon(Icons.add_a_photo),
           )
         ],
